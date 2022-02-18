fd6d70b58903e20683f0aea3a56874f24806e21c
Mr-Milk <yb97643@um.edu.mo>
2022-02-18 11:07:32 +0800

    remove unused code

--- toolbox/GPUselect.py
+++ toolbox/GPUselect.py
@@ -1,22 +1,4 @@
-# import subprocess, re
 import numpy as np
-
-# def pick_gpu_lowest_memory():
-#     output = subprocess.Popen("nvidia-smi", stdout=subprocess.PIPE, shell=True).communicate()[0]
-#     output=output.decode("ascii")
-#     gpu_output = output[output.find("Memory-Usage"):]
-#         # lines of the form
-#         # |    0      8734    C   python                                       11705MiB |
-#     memory_regex = re.compile(r"[|]\s+?\D+?.+[ ](?P<gpu_memory>\d+)MiB /")
-#     rows = gpu_output.split("\n")
-#     result=[]
-#     for row in gpu_output.split("\n"):
-#         m = memory_regex.search(row)
-#         if not m:
-#             continue
-#         gpu_memory = int(m.group("gpu_memory"))
-#         result.append(gpu_memory)
-#     return np.argsort(result)[0]
 
 
 def pick_gpu_lowest_memory():
