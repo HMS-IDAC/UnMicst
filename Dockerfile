dcf79d48a967cbd1916aaea75d7112b27c6dac80
Mr-Milk <zym.zym1220@gmail.com>
2022-02-18 11:13:14 +0800

    Rework the code of GPU selection (#12)
    * Rework the code of GPU selection
    
    * remove unused code

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM tensorflow/tensorflow:2.7.1-gpu
-RUN pip install scikit-image matplotlib tifffile czifile==2019.7.2 nd2reader==3.2.3
+RUN pip install scikit-image matplotlib tifffile pynvml czifile==2019.7.2 nd2reader==3.2.3
 
 COPY . /app
 RUN curl -f -o /app/models/nucleiDAPILAMIN/model.ckpt.data-00000-of-00001 https://mcmicro.s3.amazonaws.com/models/unmicst2/model.ckpt.data-00000-of-00001
--- toolbox/GPUselect.py
+++ toolbox/GPUselect.py
@@ -1,19 +1,22 @@
-import subprocess, re
 import numpy as np
 
+
 def pick_gpu_lowest_memory():
-    output = subprocess.Popen("nvidia-smi", stdout=subprocess.PIPE, shell=True).communicate()[0]
-    output=output.decode("ascii")
-    gpu_output = output[output.find("Memory-Usage"):]
-        # lines of the form
-        # |    0      8734    C   python                                       11705MiB |
-    memory_regex = re.compile(r"[|]\s+?\D+?.+[ ](?P<gpu_memory>\d+)MiB /")
-    rows = gpu_output.split("\n")
-    result=[]
-    for row in gpu_output.split("\n"):
-        m = memory_regex.search(row)
-        if not m:
-            continue
-        gpu_memory = int(m.group("gpu_memory"))
-        result.append(gpu_memory)
-    return np.argsort(result)[0]
+    try:
+        import pynvml as nv
+    except ImportError:
+        raise ImportError("pynvml not found")
+
+    nv.nvmlInit()
+    gpu_count = nv.nvmlDeviceGetCount()
+    gpu_idle_memory = []
+    for i in range(gpu_count):
+        handle = nv.nvmlDeviceGetHandleByIndex(i)
+        try:
+            memory = nv.nvmlDeviceGetMemoryInfo(handle)
+            gpu_idle_memory.append(memory.total - memory.used)
+        except nv.NVMLError:
+            gpu_idle_memory.append(0.0)
+
+    # return the GPU that has the largest memory left
+    return np.argsort(gpu_idle_memory)[-1]
